89b02cbb7e05fe7bf8a434b07988e31332423ecb
meet-tola <wasiusikiru7@gmail.com>
2025-02-02 23:03:26 +0100

    added register endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { config } from "../config/app.config";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import passport from "passport";
+import { registerSchema } from "../validation/auth.validation";
+import { registerUserService } from "../services/auth.service";
+import { HTTPSTATUS } from "../config/http.config";
 
 export const googleLoginCallback = asyncHandler(
     async (req: Request, res: Response) => {
@@ -16,5 +19,20 @@ export const googleLoginCallback = asyncHandler(
       return res.redirect(
         `${config.FRONTEND_ORIGIN}/workspace/${currentWorkspace}`
       );
+    }
+  );
+
+export const register = asyncHandler(
+    async (req: Request, res: Response) => {
+      const body = registerSchema.parse({
+        ...req.body,
+      });
+
+      await registerUserService(body);
+      
+  
+      return res.status(HTTPSTATUS.CREATED).json({
+        message: "User registered successfully",
+      });
     }
   );
